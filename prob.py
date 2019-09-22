8dd071ebc582c0727a4e9e13b8dd1353d66d3a17
Aaditya Gupta <addyrockx96@gmail.com>
2019-09-22 14:09:46 +0530

    temp variable usage removed

--- prob.py
+++ prob.py
@@ -1,12 +1,9 @@
 def no_of_paths(x,y):
-    print(x,y)
 
     if x == 1 or y == 1:
         return 1
 
-    temp = (no_of_paths(x-1, y) + no_of_paths(x, y-1))
-
-    return temp
+    return (no_of_paths(x-1, y) + no_of_paths(x, y-1))
 
 
 print(no_of_paths(4,4))
