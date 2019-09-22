ff5643e4e9293c9119d360823ce0ef80067fc3d2
Aaditya Gupta <addyrockx96@gmail.com>
2019-09-22 14:38:46 +0530

    Solved

--- prob.py
+++ prob.py
@@ -1,9 +1,14 @@
-def no_of_paths(x,y):
+counter = 0
 
+def no_of_paths(x,y):
+    global counter
+    counter+=1
+
+    # print(x,y)
     if x == 1 or y == 1:
         return 1
+    (no_of_paths(x-1, y) + no_of_paths(x, y-1))
+    
+    return counter - 1
 
-    return (no_of_paths(x-1, y) + no_of_paths(x, y-1))
-
-
-print(no_of_paths(4,4))
+print(no_of_paths(4,3))
